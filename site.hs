a08dd64db738d39183dfe018b045bdb0e4a150f9
Andrey Komarov <taukus@gmail.com>
2013-10-24 22:29:10 +0400

    started sqrt-decomposition

--- site.hs
+++ site.hs
@@ -1,7 +1,9 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Monoid (mappend)
+import           Data.Monoid (mappend, (<>))
 import           Hakyll
+import Text.Pandoc
+import qualified Data.Map as M
 
 
 --------------------------------------------------------------------------------
@@ -10,6 +12,16 @@ config :: Configuration
 config = defaultConfiguration 
     { deployCommand = "scp -r _site/* rain:~/public_html" 
     }
+
+pandocOptions :: WriterOptions
+pandocOptions = defaultHakyllWriterOptions{ writerHTMLMathMethod = MathJax "" }
+
+mathCtx :: Context a
+mathCtx = field "mathjax" $ \item -> do
+    metadata <- getMetadata $ itemIdentifier item
+    return $ if "mathjax" `M.member` metadata
+                  then "<script type=\"text/javascript\" src=\"http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML\"></script>"
+                  else ""
 
 main :: IO ()
 main = hakyllWith config $ do
@@ -24,7 +36,7 @@ main = hakyllWith config $ do
     match (fromList ["contact.markdown"]) $ do
         route   $ setExtension "html"
         compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= loadAndApplyTemplate "templates/default.html" (mathCtx <> defaultContext)
             >>= relativizeUrls
 
     match "src/*" $ do
@@ -37,24 +49,24 @@ main = hakyllWith config $ do
 
     match "posts/*" $ do
         route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
+        compile $ pandocCompilerWith defaultHakyllReaderOptions pandocOptions
+            >>= loadAndApplyTemplate "templates/post.html"    (mathCtx <> postCtx)
+            >>= loadAndApplyTemplate "templates/default.html" (mathCtx <> postCtx)
             >>= relativizeUrls
 
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Архив"            `mappend`
-                    defaultContext
-
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
+--    create ["archive.html"] $ do
+--        route idRoute
+--        compile $ do
+--            posts <- recentFirst =<< loadAll "posts/*"
+--            let archiveCtx =
+--                    listField "posts" postCtx (return posts) `mappend`
+--                    constField "title" "Архив"            `mappend`
+--                    defaultContext
+--
+--            makeItem ""
+--                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+--                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+--                >>= relativizeUrls
 
 
     match "index.html" $ do
@@ -68,7 +80,7 @@ main = hakyllWith config $ do
 
             getResourceBody
                 >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
+                >>= loadAndApplyTemplate "templates/default.html" (mathCtx <> indexCtx)
                 >>= relativizeUrls
 
     match "templates/*" $ compile templateCompiler
